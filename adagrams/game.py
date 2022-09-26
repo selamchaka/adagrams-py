39dc166018622383f32fbfaaca31583ee4493d33
Selam Chaka <selblessed@gmail.com>
2022-09-26 19:06:16 -0400

    passed wave-1 first 2 tests

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,42 @@
+import random
+LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
 def draw_letters():
-    pass
+    random_letters=[]
+    while len(random_letters) <10:
+        r = random.choice(list(LETTER_POOL.keys()))
+        
+        random_letters.append(r)
+    return random_letters
+    
+    
 
 def uses_available_letters(word, letter_bank):
     pass
